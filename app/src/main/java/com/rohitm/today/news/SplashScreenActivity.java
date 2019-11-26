6b117fd10a52ef1883860d9f5ac7457d06c67cc7
Rohit Maurya <rohit.digiinterface@gmail.com>
2019-11-26 13:01:15 +0530

    Initial commit

---
+++ app/src/main/java/com/rohitm/today/news/SplashScreenActivity.java
@@ -0,0 +1,17 @@
+package com.rohitm.today.news;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+
+    }
+}
---
+++ app/src/main/java/com/rohitm/today/news/activities/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.rohitm.today.news.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.rohitm.today.news.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
